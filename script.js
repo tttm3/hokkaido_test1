3b186dde0979c9a40b280a59b28e5895ceb4922a
tttm3 <songj8607@gmail.com>
2025-11-20 04:05:57 +0900

    svg実施

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 地図の初期化とインタラクティブ機能
 
 // 使用する地図画像のパス
-const MAP_IMAGE_PATH = 'images/hokkaido/北海道エリア_文字なし.png';
+const MAP_IMAGE_PATH = 'images/hokkaido/北海道エリア_文字なし.svg';
 const HAS_MAP_IMAGE = true;
 const MAP_VIEWBOX = { width: 1152, height: 896 };
 
